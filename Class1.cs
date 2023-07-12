74df7bbf00f5390c2caae5568a9ed89504f237b6
Augmento <Augmento@DESKTOP-TMMTNJV>
2023-07-12 19:09:38 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LengthHelperLibrary
+{
+    public class LengthHelper
+    {
+        public int FindLength(string input)
+        {
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            return input.Length;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LengthHelperConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LengthHelperLibrary.LengthHelper lengthHelper = new LengthHelperLibrary.LengthHelper();
+
+            string[] inputs = { "", "123", "123456789010", null };
+
+            foreach (string input in inputs)
+            {
+                try
+                {
+                    int length = lengthHelper.FindLength(input);
+                    Console.WriteLine($"Length of '{input}' is: {length}");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Input cannot be null.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
