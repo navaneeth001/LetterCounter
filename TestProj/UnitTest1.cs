91edc00cb26981bef43c4f85f4d90ef744b53808
Navaneeth Prakash <navaneethpqln@gmail.com>
2023-07-14 10:16:24 +0530

    unit tests

---
+++ TestProj/UnitTest1.cs
@@ -0,0 +1,44 @@
+using Xunit;
+using LengthHelperLibrary;
+
+namespace LengthHelperTests
+{
+    public class LengthHelperTests
+    {
+        [Fact]
+        public void FindLength_WithEmptyString_ReturnsZero()
+        {
+            // Arrange
+            LengthHelper lengthHelper = new LengthHelper();
+
+            // Act
+            int length = lengthHelper.FindLength("");
+
+            // Assert
+            Assert.Equal(0, length);
+        }
+
+        [Fact]
+        public void FindLength_WithNonEmptyString_ReturnsLength()
+        {
+            // Arrange
+            LengthHelper lengthHelper = new LengthHelper();
+
+            // Act
+            int length = lengthHelper.FindLength("123");
+
+            // Assert
+            Assert.Equal(3, length);
+        }
+
+        [Fact]
+        public void FindLength_WithNullString_ThrowsArgumentNullException()
+        {
+            // Arrange
+            LengthHelper lengthHelper = new LengthHelper();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => lengthHelper.FindLength(null));
+        }
+    }
+}
